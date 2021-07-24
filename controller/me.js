4c6538bd8b529d921fd27089e22cdb577ca428f4
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2021-07-24 18:56:58 +0800

    adding a news api

--- controller/me.js
+++ controller/me.js
@@ -1,9 +1,34 @@
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('98e5e4e958fa47eb97c242794e4165d8');
+
+
 exports.getMe = async (req, res) =>{
 
+    // newsapi.v2.topHeadlines({
+    //     language: 'en',
+    //     country: 'ph',
+    // })
+    // .then(response => {
+    //     console.log(response);
+    // });
+      
+
     try{
-        res.render('home/me',{
-            pageTitle: 'Roniel'
+        newsapi.v2.topHeadlines({
+            language: 'en',
+            country: 'ph',
         })
+        .then((response) => {
+            res.render('home/me',{
+                pageTitle: 'Roniel',
+                response
+            })
+
+            console.log(response)
+        })
+
+
+   
     }catch(err){
         console.log(err);
         res.render('404', {
