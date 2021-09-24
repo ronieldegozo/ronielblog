4bb2acc597231997a87ceb7e50ca043357b8aa9c
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2021-09-24 12:46:24 +0800

    email send grind api

--- controller/email.js
+++ controller/email.js
@@ -1,9 +1,21 @@
 const nodemailer = require('nodemailer');
+const sendGridTransport = require('nodemailer-sendgrid-transport');
+
+const transporter = nodemailer.createTransport(sendGridTransport({
+  auth:{
+      api_key: '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' //API KEY
+  }
+}));
 
 exports.postEmail =  (req, res) => {
     const output = `
-      <p><center><h1>Thank you for Contacting Roniel Blog site</h1></center></p>
-
+      <p>
+        <center>
+          <h1>Thank you for Contacting Roniel Blog site</h1>
+          <img class="card-img-top" src="https://seeklogo.com/images/R/react-logo-7B3CE81517-seeklogo.com.png" alt="Rouge">
+          <img class="card-img-top" src="https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png" alt="Rouge">
+        </center>
+      </p>
       <h3>Your Contact Information</h3>
       <ul>  
         <li>Subject: ${req.body.subject}</li>
@@ -15,44 +27,11 @@ exports.postEmail =  (req, res) => {
       <h3>Message</h3>
       <p>${req.body.message}</p>
     `;
-  
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-      service: 'Gmail',
-      port: 587,
-      secure: false, // true for 465, false for other ports
-      auth: {
-          user: 'lauritosamber@gmail.com', // generated ethereal user
-          pass: 'Samber061617'  // generated ethereal password
-      },
-      tls:{
-        rejectUnauthorized:false
-      }
+     transporter.sendMail({
+      to: `${req.body.email}`,
+      from: 'lauritosamber@gmail.com',
+      subject: `Blog Roniel , ${req.body.subject}`,
+      html: output,
     });
-  
-    // setup email data with unicode symbols
-    let mailOptions = {
-
-        from: '"Blog Roniel" <lauritosamber@gmail.com>', // sender address
-        to: `${req.body.email}`,
-        
-
-        // list of receivers
-        subject: `Blog Roniel , ${req.body.subject}`,
-        // subject: 'New Era University inquiry', // Subject line
-        text: 'Hello world?', // plain text body
-        html: output, // html body
-    };
-  
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);   
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        
-        res.redirect('/');
-    });
-}
-
+    res.redirect('/');
+};
